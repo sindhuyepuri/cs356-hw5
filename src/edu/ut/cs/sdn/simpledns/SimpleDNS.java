222e93bbeddbf24881afc3f7d9c5c877d4023db9
Sindhu Yepuri <sindhu@wireless-10-145-42-70.public.utexas.edu>
2022-11-30 00:53:31 -0600

    kms

--- src/edu/ut/cs/sdn/simpledns/SimpleDNS.java
+++ src/edu/ut/cs/sdn/simpledns/SimpleDNS.java
@@ -15,7 +15,8 @@ import java.net.DatagramPacket;
 public class SimpleDNS 
 {
 	public static final int PORT = 8053;
-	public static final int BUFFER_SZ = 65_507;
+	public static final int ROOT_PORT = 53;
+	public static final int BUFFER_SZ = 100_000;
 	public static void main(String[] args)
 	{
 		Arguments arguments = Arguments.parseArguments(args);
@@ -31,56 +32,57 @@ public class SimpleDNS
 	private SimpleDNS(String rootServerIp, CSVReader reader) {
 		this.reader = reader;
 		try {
-			this.root = new DatagramSocket();
+			// this.root = new DatagramSocket();
 			this.root_addr = InetAddress.getByName(rootServerIp);
-			this.root.connect(this.root_addr, SimpleDNS.PORT);
-		} catch (SocketException e) {
-			System.exit(-1);
+			// this.root.connect(root_addr, SimpleDNS.PORT);
+		// } catch (SocketException e) {
+		//	System.exit(-1);
 		} catch (UnknownHostException e) {
 			System.exit(-1);
 		}
 	}
 
 	private void run() {
-		System.out.println("******** RUN");
-		try (DatagramSocket ds = new DatagramSocket(SimpleDNS.PORT) ) {
+		try (DatagramSocket ds = new DatagramSocket(SimpleDNS.PORT)) {
+			// ds.connect(root_addr, PORT);
+			// System.out.println("***** SOCKET local socket address: " + ds.getLocalSocketAddress());
+			// System.out.println("***** SOCKET remote socket address: " + ds.getRemoteSocketAddress());
 			byte[] data = new byte[SimpleDNS.BUFFER_SZ];
 			DatagramPacket udpPacket = new DatagramPacket(data, SimpleDNS.BUFFER_SZ);
 			
 			// keep checking for new packets to process
 			while (true) {
+				// receive a packet for which we need to generate a response
 				ds.receive(udpPacket);
-				System.out.println("******** RECEIVED PACKET:\n" + udpPacket);
-;				DNS dns = DNS.deserialize(udpPacket.getData(), udpPacket.getLength());
-				if (dns.isQuery() && dns.getOpcode() == DNS.OPCODE_STANDARD_QUERY) {
-					// boolean recursion = dns.isRecursionDesired();
-					DNSQuestion question = dns.getQuestions().get(0);
-					switch (question.getType()) {
-						case DNS.TYPE_A:
-						case DNS.TYPE_AAAA:
-						case DNS.TYPE_CNAME:
-						case DNS.TYPE_NS:
-							// DNSResourceRecord result = handleAQuery(dns, question);
-							// forward packet
-							System.out.println("******** FORWARDING PACKET:\n" + udpPacket);
-							System.out.println("\n\n");
-							udpPacket.setSocketAddress(root.getRemoteSocketAddress());
-							udpPacket.setAddress(this.root_addr);
-							this.root.send(udpPacket);
-							// System.out.println("**** ROOT ADDRESS: " +this.root_addr.toString());
-							// System.out.println("**** ROOT SOCKET ADDRESS: " +root.getRemoteSocketAddress().toString());
+				DNS dns = DNS.deserialize(udpPacket.getData(), udpPacket.getLength());
 
-							byte[] responseData = new byte[SimpleDNS.BUFFER_SZ];
-							DatagramPacket response = new DatagramPacket(responseData, SimpleDNS.BUFFER_SZ);
-							this.root.receive(response);							
-							System.out.println("******** RECEIVED RESPONSE:\n" + response);
-							System.out.println("\n\n");
-							break;
-						// we silently ignore all other problems
-						default:
-							throw new UnsupportedOperationException();
-					}
-				} // fi : silently drop all other packets
+				// we only consider standard queries
+				if (!dns.isQuery() || dns.getOpcode() != DNS.OPCODE_STANDARD_QUERY) continue;
+
+				boolean recursion = dns.isRecursionDesired();
+
+				// Implementation for handling multiple questions is not required.
+				// we only consider the first question
+				DNSQuestion question = dns.getQuestions().get(0);
+
+				switch (question.getType()) {
+					case DNS.TYPE_A:
+					case DNS.TYPE_AAAA:
+					case DNS.TYPE_CNAME:
+					case DNS.TYPE_NS:
+						udpPacket.setAddress(this.root_addr);
+						udpPacket.setPort(SimpleDNS.ROOT_PORT);
+						// udpPacket.setSocketAddress(this.root.getRemoteSocketAddress());
+						ds.send(udpPacket);
+						byte[] responseData = new byte[SimpleDNS.BUFFER_SZ];
+						DatagramPacket response = new DatagramPacket(responseData, SimpleDNS.BUFFER_SZ);
+						ds.receive(response);
+						System.out.println("******** RESPONSE: \n"+DNS.deserialize(response.getData(), response.getLength()));
+						break;
+					default:
+						// we silently ignore all other question types
+						break;
+				}
 				
 			}
 		} catch (SocketException se) {
